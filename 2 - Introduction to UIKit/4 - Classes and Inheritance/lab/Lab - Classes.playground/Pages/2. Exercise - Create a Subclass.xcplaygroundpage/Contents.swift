e7365153d008ecb1d3047fa1a6012efbfacb052e
Tyler Donohue <donohue_tyler@yahoo.com>
2018-09-14 09:58:02 -0600

    added 3/4 on class lab

--- 2 - Introduction to UIKit/4 - Classes and Inheritance/lab/Lab - Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/4 - Classes and Inheritance/lab/Lab - Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
@@ -19,21 +19,46 @@ class Spaceship {
     func wasHit() {
         health -= 5
     }
+    
 }
 /*:
  Define a new class `Fighter` that inherits from `Spaceship`. Add a variable property `weapon` that defaults to an empty string and a variable property `remainingFirePower` that defaults to 5.
  */
-
+class Fighter: Spaceship {
+    var weapon = ""
+    var remainingFirePower = 5
+    
+    func fire() {
+        if remainingFirePower > 0 {
+            remainingFirePower -= 1
+        } else {
+            print("You have no more fire power.")
+}
+    }
+}
 
 /*:
  Create a new instance of `Fighter` called `destroyer`. A `Fighter` will be able to shoot incoming objects to avoid colliding with them. After initialization, set `weapon` to "Laser" and `remainingFirePower` to 10. Note that since `Fighter` inherits from `Spaceship`, it also has properties for `name`, `health`, and `position`, and has methods for `moveLeft()`, `moveRight()`, and `wasHit()` even though you did not specifically add them to the declaration of `Fighter`. Knowing that, set `name` to "Destroyer," print `position`, then call `moveRight()` and print `position` again.
  */
+        class Destroyer: Fighter {
+            init(laser: String) {
+               
+                self.laser = laser
+            }
+            var laser: String
+          
+}
+let destroyer = Destroyer(laser: "pewpew")
+destroyer.moveLeft()
+destroyer.moveLeft()
+print(destroyer.position)
 
 
 /*:
  Try to print `weapon` on `falcon`. Why doesn't this work? Provide your answer in a comment or a print statement below, and remove any code you added that doesn't compile.
  */
-
+// print(falcon.weapon)
+// I need to specify the subclass that has the var weapon as SpaceShip does not, i also have not set falcon to  = SpaceShip on this page.
 
 /*:
  Add a method to `fighter` called `fire()`. This should check to see if `remainingFirePower` is greater than 0, and if so, should decrement `remainingFirePower` by one. If `remainingFirePower` is not greater than 0, print "You have no more fire power." Call `fire()` on `destroyer` a few times and print `remainingFirePower` after each method call.
