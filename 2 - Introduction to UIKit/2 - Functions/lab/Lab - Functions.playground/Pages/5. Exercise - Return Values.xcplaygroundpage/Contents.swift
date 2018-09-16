55557171507d0dc527330a989d963b7ed13e4e09
Tyler Donohue <donohue_tyler@yahoo.com>
2018-09-16 11:28:39 -0600

    func lab fixed/updated

--- 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -4,10 +4,11 @@
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Dan" the return value might be "Hi, Dan! How are you?" Use the function and print the result.
  */
 func greeting(hi: String, yo: String) -> String {
-    return hi
-    print("Oh I didn't se you there \(hi)")
+    return "Oh, \(hi) \(yo)"
 }
-greeting(hi: "Why hello there", yo: "Dan")
+let sentance = greeting(hi: "Why hello there", yo: "Dan")
+print(sentance)
+
 /*:
  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
  */
@@ -15,5 +16,6 @@ func math(num1: Int, to num2: Int) -> Int {
     return num1 * num2 + 2
     
 }
-math(num1: 2, to: 3)
+var solve = math(num1: 3, to: 4)
+print(solve)
 //: [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
