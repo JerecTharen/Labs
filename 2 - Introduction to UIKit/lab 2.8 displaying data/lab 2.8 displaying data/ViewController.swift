1ab28c88874183e6f5c0f55ceb740945f0c15523
Tyler Donohue <donohue_tyler@yahoo.com>
2018-09-19 08:43:45 -0600

    lab 2.8 upload

---
+++ 2 - Introduction to UIKit/lab 2.8 displaying data/lab 2.8 displaying data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab 2.8 displaying data
+//
+//  Created by Tyler Donohue on 9/19/18.
+//  Copyright © 2018 Tyler Donohue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 2 - Introduction to UIKit/lab 2.8 displaying data/lab 2.8 displaying dataTests/lab_2_8_displaying_dataTests.swift
@@ -0,0 +1,34 @@
+//
+//  lab_2_8_displaying_dataTests.swift
+//  lab 2.8 displaying dataTests
+//
+//  Created by Tyler Donohue on 9/19/18.
+//  Copyright © 2018 Tyler Donohue. All rights reserved.
+//
+
+import XCTest
+@testable import lab_2_8_displaying_data
+
+class lab_2_8_displaying_dataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
