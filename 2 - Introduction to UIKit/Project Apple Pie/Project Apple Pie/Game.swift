572841e036c9429c53aa3e290739f4f587346354
Tyler Donohue <donohue_tyler@yahoo.com>
2018-09-21 10:31:04 -0600

    added game.swift

---
+++ 2 - Introduction to UIKit/Project Apple Pie/Project Apple Pie/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  Project Apple Pie
+//
+//  Created by Tyler Donohue on 9/21/18.
+//  Copyright © 2018 Tyler Donohue. All rights reserved.
+//
+
+import Foundation
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
