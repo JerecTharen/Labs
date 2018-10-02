4fff3728b577d3589a50aa2003d679f6848dc69a
Tyler Donohue <donohue_tyler@yahoo.com>
2018-10-02 09:46:56 -0600

    90% done with lab 3.4

--- 3 - Navigation and Workflows/4 - Scope/lab/Lab - Scope.playground/Pages/1. Exercise - Scope.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/4 - Scope/lab/Lab - Scope.playground/Pages/1. Exercise - Scope.xcplaygroundpage/Contents.swift
@@ -8,7 +8,7 @@ for _ in 0..<10 {
     print("The value of foo is \(foo)")
 }
 //print("The value of foo is \(foo)")
-
+// you got rid of the end of the loop, 0 in what. in 3. how many times????
 
 /*:
  Using a comment or print statement, describe why both print statements below compile when similar-looking code did not compile above. In what scope is `x` defined, and in what scope is it modified? In contrast, in what scope is `foo` defined and used?
@@ -20,16 +20,35 @@ for _ in 0..<10 {
 }
 print("The final value of x is \(x)")
 
-
+// var x is a global scope while foo ahs no global scope to wroap and is stuck in its local scope. forever >:3
 /*:
  In the body of the function `greeting` below, use variable shadowing when unwrapping `greeting`. If `greeting` is successfully unwrapped, print a statement that uses the given greeting to greet the given name (i.e. if `greeting` successfully unwraps to have the value "Hi there" and `name` is `Sara`, print "Hi there, Sara."). Otherwise, use "Hello" to print a statement greeting the given name. Call the function twice, once passing in a value for greeting, and once passing in `nil`.
  */
 func greeting(greeting: String?, name: String) {
-    
+    if let greeting = greeting {
+        print("\(greeting) \(name), TIME 2 DYE")
+        return
+    }
 }
+var greet = greeting(greeting: "Oh hello", name: "JIM")
+greet
 /*:
  Create a class called `Car`. It should have properties for `make`, `model`, and `year` that are of type `String`, `String`, and `Int`, respectively. Since this is a class, you'll need to write your own memberwise initializer. Use shadowing when naming parameters in your initializer.
  */
-
+class Car {
+    var make: String
+    var model: String
+    var year: Int
+    init(make: String, model: String, year: Int) {
+        self.make = make
+        self.model = model
+        self.year = year
+    }
+    
+}
+let car = Car(make: "Jim", model: "Jam", year: 9)
+print(car.make)
+print(car.model)
+print(car.year)
 
 //: page 1 of 2  |  [Next: App Exercise - Step Competition](@next)
