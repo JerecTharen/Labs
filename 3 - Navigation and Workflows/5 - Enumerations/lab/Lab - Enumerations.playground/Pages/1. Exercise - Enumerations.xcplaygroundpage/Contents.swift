2a1d0ca42aed2066dbbfbb15970a49aa89b16d2d
Tyler Donohue <donohue_tyler@yahoo.com>
2018-10-03 08:24:10 -0600

    enum practice and info

--- 3 - Navigation and Workflows/5 - Enumerations/lab/Lab - Enumerations.playground/Pages/1. Exercise - Enumerations.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/5 - Enumerations/lab/Lab - Enumerations.playground/Pages/1. Exercise - Enumerations.xcplaygroundpage/Contents.swift
@@ -7,15 +7,20 @@ enum Suit {
     case clubs, spades, diamonds, hearts
 }
 var cardInHands = Suit.hearts
-/*:
- Imagine you are being shown a card trick and have to draw a card and remember the suit. Create a variable instance of `Suit` called `cardInHand` and assign it to the `hearts` case. Print out the instance.
- */
 print(cardInHands)
+
+
+/*
+ 
+ 
+ 
+*/
 
 /*:
  Now imagine you have to put back the card you drew and draw a different card. Update the variable to be a spade instead of a heart.
  */
-
+var cardInHands = Suit.spades
+print(cardInHands)
 
 /*:
  Imagine you are writing an app that will display a fun fortune (i.e. something like "You will soon find what you seek.") based on cards drawn. Write a function called `getFortune(cardSuit:)` that takes a parameter of type `Suit`. Inside the body of the function, write a switch statement based on the value of `cardSuit`. Print a different fortune for each `Suit` value. Call the function a few times, passing in different values for `cardSuit` each time.
