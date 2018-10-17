03353f888b94819d71f15504473ee33e32d4c12d
Tyler Donohue <42743272+TyDono@users.noreply.github.com>
2018-10-17 08:59:48 -0600

    Add files via upload

---
+++ projetcs/Light/Light/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Tyler Donohue on 9/11/18.
+//  Copyright © 2018 Tyler Donohue. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    var lightOn = true
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI()  {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
