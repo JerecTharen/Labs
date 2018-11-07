8f7161017d18465880f142f0154a10cbada993f9
Tyler Donohue <donohue_tyler@yahoo.com>
2018-11-07 10:21:01 -0700

    on mediawithinfo

--- 4 - Tables and Persistence/8 - System View Controllers/lab/HomeFurniture/HouseFurniture/FurnitureDetailViewController.swift
+++ 4 - Tables and Persistence/8 - System View Controllers/lab/HomeFurniture/HouseFurniture/FurnitureDetailViewController.swift
@@ -1,7 +1,7 @@
 
 import UIKit
 
-class FurnitureDetailViewController: UIViewController {
+class FurnitureDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var furniture: Furniture?
     
@@ -31,10 +31,45 @@ class FurnitureDetailViewController: UIViewController {
     }
     
     @IBAction func choosePhotoButtonTapped(_ sender: Any) {
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+            
+            if let selectedImage = info[UINavigationController] as? UIImage {
+                imageView.image = selectedImage
+                dismiss(animated: true, completion: nil)
+            }
+        }
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "camera", style: .default, handler: { (action) in
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(cameraAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "photo library", style: .default, handler: { (action) in
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        
+        
         
         let cancel = UIAlertAction(title: nil, style: .cancel, handler: nil)
         alertController.addAction(cancel)
+        
+        let chooseImage = UIAlertAction(title: "pick an image", style: .default, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>)
+        alertController.addAction(<#T##action: UIAlertAction##UIAlertAction#>)
+        let useCamera = UIAlertAction(title: "can we use camera b0ss?", style: .default, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>)
+        alertController.addAction(<#T##action: UIAlertAction##UIAlertAction#>)
+        present(alertController, animated: true, completion: nil)
+        
     }
 
     @IBAction func actionButtonTapped(_ sender: Any) {
