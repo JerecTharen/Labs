4a5aca55a431b757fc56c2e7ffb1023f08905c18
Tyler Donohue <donohue_tyler@yahoo.com>
2018-11-08 10:22:08 -0700

    show and hide date pickers

--- 4 - Tables and Persistence/9 - Complex Input Screens/ jeFF/ jeFF/AddRegistrationTableViewController.swift
+++ 4 - Tables and Persistence/9 - Complex Input Screens/ jeFF/ jeFF/AddRegistrationTableViewController.swift
@@ -12,6 +12,22 @@ class AddRegistrationTableViewController: UITableViewController {
     
     //mark Properties
     
+    // general
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    
+    var isCheckInDatePickerShown: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerShown
+        }
+    }
+    
+    var isCheckOutDatePickerShown: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerShown
+        }
+    }
+    
     // Outlets
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -23,16 +39,18 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet weak var checkOutDatePicker: UIDatePicker!
     
     // mark View Life Cycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-let midnightToday = Calendar.current.startOfDay(for: Date())
+        let midnightToday = Calendar.current.startOfDay(for: Date())
         checkInDatePicker.minimumDate = midnightToday
         checkInDatePicker.date = midnightToday
         
         updateDateViews()
-    
+        
     }
+    
+    // init sumitng here??
     
     func updateDateViews() {
         checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(86400)
@@ -70,6 +88,65 @@ let midnightToday = Calendar.current.startOfDay(for: Date())
     }
     
     // MARK: - Navigation
+    
+    // tablView delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row - 1):
+            
+            if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+            } else if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+                isCheckInDatePickerShown = true
+            } else {
+                isCheckInDatePickerShown = true
+            }
+            
+            
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row - 1):
+            if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+            } else if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+                isCheckOutDatePickerShown = true
+            } else {
+                isCheckOutDatePickerShown = true
+            }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        default: break
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row):
+            if isCheckInDatePickerShown {
+                return 216.0
+            }else {
+                return 0
+            }
+        
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row):
+            if isCheckOutDatePickerShown {
+                return 216.0
+            }else {
+                return 0
+            }
+        default: return 44.0
+        }
+    }
+    
+
+
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -78,3 +155,4 @@ let midnightToday = Calendar.current.startOfDay(for: Date())
     }
 
 }
+
