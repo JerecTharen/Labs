67a2a7ba790bf81f61a99b216a0e3ee37d851126
Tyler Donohue <donohue_tyler@yahoo.com>
2018-11-12 10:11:06 -0700

    so far, so good

---
+++ 4 - Tables and Persistence/lab 4.4 ScrollViews/lab 4.4 ScrollViews/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  lab 4.4 ScrollViews
+//
+//  Created by Tyler Donohue on 11/12/18.
+//  Copyright © 2018 Tyler Donohue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var scrollViewOutlet: UIScrollView!
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateZoomFor(size: view.bounds.size)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageViewOutlet
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageViewOutlet.bounds.width
+        let heightScale = size.height / imageViewOutlet.bounds.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollViewOutlet.minimumZoomScale = minScale
+    }
+
+
+}
+
