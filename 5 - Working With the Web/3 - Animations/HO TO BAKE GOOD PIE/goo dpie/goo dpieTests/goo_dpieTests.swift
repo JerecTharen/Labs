2ba7741042ed271a62d244be3df4f457f895c51b
Tyler Donohue <donohue_tyler@yahoo.com>
2018-11-29 10:35:11 -0700

    good pie

---
+++ 5 - Working With the Web/3 - Animations/HO TO BAKE GOOD PIE/goo dpie/goo dpieTests/goo_dpieTests.swift
@@ -0,0 +1,34 @@
+//
+//  goo_dpieTests.swift
+//  goo dpieTests
+//
+//  Created by Tyler Donohue on 11/29/18.
+//  Copyright © 2018 Tyler Donohue. All rights reserved.
+//
+
+import XCTest
+@testable import goo_dpie
+
+class goo_dpieTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
