06233849645f9ff5b65856ad15735b924d7df5e2
Tyler Donohue <donohue_tyler@yahoo.com>
2018-12-21 08:07:26 -0700

    arbitrary loads

---
+++ junk test for me to work on/progamaticallyCoCoaHeads/progamaticallyCoCoaHeads/view.swift
@@ -0,0 +1,52 @@
+//
+//  view.swift
+//  progamaticallyCoCoaHeads
+//
+//  Created by Tyler Donohue on 12/20/18.
+//  Copyright © 2018 Tyler Donohue. All rights reserved.
+//
+
+import Foundation
+import UIKit
+final class HomeView: UIView {
+//
+//    let square = UIView()
+//    let tableiView = UITableView
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        configureAppearance()
+//        configureLayout()
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder) has not been inplemented")
+//    }
+//    private func configureAppearance() {
+//        backgroundColor = .white
+//        square.backgroundColor = .blue
+//    }
+//
+//   private func configureLayout() {
+//    addSubview(square)
+//        square.translatesAutoresizingMaskIntoConstraints = false
+//
+//        NSLayoutConstraint.activate([
+//        square.centerXAnchor.constraint(equalToSystemSpacingAfter: centerXAnchor)
+//
+//        ])
+//    }
+//
+//    extension UIView {
+//        func addAutoLayoutSubView(_ subView: UIView) {
+//            addSubview(subView)
+//            subView.translatesAutoresizingMaskIntoConstraints
+//        }
+//    }
+//
+//    
+}
+
+
+
+
