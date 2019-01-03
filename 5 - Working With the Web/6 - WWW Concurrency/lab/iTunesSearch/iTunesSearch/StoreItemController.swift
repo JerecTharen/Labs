3fc0172896d4a98b9ac7a7879c7c42d2673d4de8
Tyler Donohue <donohue_tyler@yahoo.com>
2019-01-02 18:41:48 -0700

    some uploads since a new file for my api's are not uploading, this is a test run please pay no mind BUT i had to change some files to make any changes now i have 8 changes? this should work bois

--- 5 - Working With the Web/6 - WWW Concurrency/lab/iTunesSearch/iTunesSearch/StoreItemController.swift
+++ 5 - Working With the Web/6 - WWW Concurrency/lab/iTunesSearch/iTunesSearch/StoreItemController.swift
@@ -8,32 +8,37 @@
 
 import Foundation
 
-func fetchItems(matching query: [String: String], completion: @escaping ([StoreItem]?) -> Void) {
+struct StoreItemController {
     
-    let baseURL = URL(string: "https://itunes.apple.com/search?")!
-    
-    guard let url = baseURL.withQueries(query) else {
+    func fetchItems(matching query: [String: String], completion: @escaping ([StoreItem]?) -> Void) {
         
-        completion(nil)
-        print("Unable to build URL with supplied queries.")
-        return
-    }
-    
-    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let baseURL = URL(string: "https://itunes.apple.com/search?")!
         
-        if let data = data,
-            let rawJSON = try? JSONSerialization.jsonObject(with: data),
-            let json = rawJSON as? [String: Any],
-            let resultsArray = json["results"] as? [[String: Any]] {
+        guard let url = baseURL.withQueries(query) else {
             
-            let storeItems = resultsArray.compactMap { StoreItem(json: $0) }
-            completion(storeItems)
-            
-        } else {
-            print("Either no data was returned, or data was not serialized.")
             completion(nil)
+            print("Unable to build URL with supplied queries.")
             return
         }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if let data = data,
+                let rawJSON = try? JSONSerialization.jsonObject(with: data),
+                let json = rawJSON as? [String: Any],
+                let resultsArray = json["results"] as? [[String: Any]] {
+                
+                let storeItems = resultsArray.compactMap { StoreItem(json: $0) }
+                completion(storeItems)
+                
+            } else {
+                print("Either no data was returned, or data was not serialized.")
+                
+                completion(nil)
+                return
+            }
+        }
+        
+        task.resume()
     }
-    task.resume()
 }
