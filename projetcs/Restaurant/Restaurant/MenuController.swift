7aca3a90060e6bd6707896065d7ec904807f88fb
Tyler Donohue <donohue_tyler@yahoo.com>
2019-01-04 10:33:17 -0700

    fixed circle bracelet error

--- projetcs/Restaurant/Restaurant/MenuController.swift
+++ projetcs/Restaurant/Restaurant/MenuController.swift
@@ -10,17 +10,26 @@ import Foundation
 
 class MenuController {
     let baseURL = URL(string: "http://localhost:8090/")!
-}
-
-func fetchCategories(completion: @escaping ([String]?) -> Void) {
     
-    let categoryURL = baseURL.appendingPathComponent("categories")
-}
-
-func fetchMenuItems(forCategory categoryName: String, completion: @escaping ([MenuItem]?) -> Void) {
+    func fetchCategories(completion: @escaping ([String]?) -> Void) {
+        
+        let categoryURL = baseURL.appendingPathComponent("categories")
+        let task = URLSession.shared.dataTask(with: categoryURL) { (data, response, error) in
+            
+        }
+        task.resume()
+    }
     
-}
-
-func submitOrder(forMenuIDs menuIds: [Int], completion: @escaping (Int?) -> Void) {
+    func fetchMenuItems(forCategory categoryName: String, completion: @escaping ([MenuItem]?) -> Void) {
+        
+        let initialMenuURL = baseURL.appendingPathComponent("menu")
+        var components = URLComponents(url: initialMenuURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
+        let menuURL = components.url!
+    }
     
+    func submitOrder(forMenuIDs menuIds: [Int], completion: @escaping (Int?) -> Void) {
+        
+        let orderURL = baseURL.appendingPathComponent("order")
+    }
 }
